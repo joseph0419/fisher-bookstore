5e0ea45641e95a3d0423b1759b977dcbfdcdc74f
Xiuyuan Yu <yuxiuyuan0324@gmail.com>
2019-04-10 19:03:13 -0400

    lab 10

---
+++ api/Migrations/20190410214630_identity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fisher.Bookstore.Api.Migrations
+{
+    public partial class identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "publishDate",
+                table: "Books",
+                newName: "PublishDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishDate",
+                table: "Books",
+                newName: "publishDate");
+        }
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Fisher.Bookstore.Api.Data;
 using Fisher.Bookstore.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -29,22 +32,38 @@ namespace Fisher.Bookstore.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BookstoreContext>(options => 
-        options.UseNpgsql(Configuration.GetConnectionString("BookstoreConnection")));
-            services.AddMvc();
+        options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext")));
+            //Add this for identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+              .AddEntityFrameworkStores<BookstoreContext>()
+              .AddDefaultTokenProviders();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+              .AddJwtBearer(jwtOptions =>
+              {
+                  jwtOptions.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                  {
+                      ValidateActor = true,
+                      ValidateAudience = true,
+                      ValidateLifetime = true,
+                      ValidIssuer = Configuration["JWTConfiguration:Issuer"],
+                      ValidAudience = Configuration["JWTConfiguration:Audience"],
+                      IssuerSigningKey = new SymmertricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTConfiguration.Key"]))
+                  };
+              });
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //Add this for identity
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseMvc();
